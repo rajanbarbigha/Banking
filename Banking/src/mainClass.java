8d13772aed49eb141184e01ac7f7f2a0f00629b0
rajan <rajanrajoxford@gmail.com>
2018-04-14 17:48:24 +0530

    added

--- Banking/src/mainClass.java
+++ Banking/src/mainClass.java
@@ -4,6 +4,8 @@ public class mainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub hello
 		System.out.println("Hello World");
+		System.out.println("Hello Rajan");
+		System.out.println("Hello Mukesh");
 	}
 
 }
