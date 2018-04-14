e2f68ed5cfff9106c1f484325612afff34d60120
rajan <rajanrajoxford@gmail.com>
2018-04-14 17:13:45 +0530

    added Interface

--- Banking/src/mainClass.java
+++ Banking/src/mainClass.java
@@ -2,7 +2,7 @@
 public class mainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub hello
 		System.out.println("Hello World");
 	}
 
